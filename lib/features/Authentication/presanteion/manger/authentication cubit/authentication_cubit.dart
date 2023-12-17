4bdab3e315c801a3afc71385b0ae616c3109a15c
abdulrhmanGhandour <abdulrhmanghandour498@gmail.com>
2023-12-17 16:55:18 +0200

    add function and auth state

---
+++ lib/features/Authentication/presanteion/manger/authentication cubit/authentication_cubit.dart
@@ -0,0 +1,43 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+part 'authentication_state.dart';
+
+class AuthenticationCubit extends Cubit<AuthenticationState> {
+  AuthenticationCubit() : super(AuthenticationInitial());
+
+   Future<void> loginUser({required email, required password}) async {
+    emit(LoginLoding());
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      emit(LoginSuccess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        emit(LoginFailure(errMessage: 'user not found'));
+      } else if (e.code == 'wrong-password') {
+        emit(LoginFailure(errMessage: 'wrong password'));
+      }
+    } catch (e) {
+      emit(LoginFailure(errMessage: 'someting is worng'));
+    }
+  }
+
+  Future<void> registerUser({required email, required password}) async {
+    emit(RegisterLoding());
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email!, password: password!);
+      emit(RegisterSuccess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        emit(RegisterFailuer(errMesage: 'weak password'));
+      } else if (e.code == 'email-already-in-use') {
+        emit(RegisterFailuer(errMesage: 'email already in use'));
+      }
+    } catch (e) {
+      emit(RegisterFailuer(errMesage: 'Somting was woring'));
+    }
+  }
+}
---
+++ lib/features/Authentication/presanteion/manger/authentication cubit/authentication_state.dart
@@ -0,0 +1,24 @@
+part of 'authentication_cubit.dart';
+
+@immutable
+sealed class AuthenticationState {}
+
+final class AuthenticationInitial extends AuthenticationState {}
+
+class LoginSuccess extends AuthenticationState {}
+
+class LoginLoding extends AuthenticationState {}
+
+class LoginFailure extends AuthenticationState {
+ final String errMessage;
+  LoginFailure({required this.errMessage});
+}
+
+class RegisterLoding extends AuthenticationState {}
+
+class RegisterSuccess extends AuthenticationState {}
+
+class RegisterFailuer extends AuthenticationState {
+ final String errMesage;
+  RegisterFailuer({required this.errMesage});
+}
